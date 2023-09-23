24a68d283840abbbea0802f2763f7605aadde063
gamedevengineer4489 <60021897+gamedevengineer4489@users.noreply.github.com>
2023-09-23 13:08:13 -0700

    Add Environment vars

---
+++ app.js
@@ -0,0 +1,80 @@
+//jshint esversion:6
+require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+var mongoose = require('mongoose');
+var encrypt = require('mongoose-encryption');
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+mongoose.connect("mongodb://localhost:27017/userDB");
+
+// const userSchema = {
+//     email: String,
+//     password: String
+// };
+
+
+
+const userSchema = new mongoose.Schema ({
+    email: String,
+    password: String
+});
+
+const secret = "Thisisourlittlesecret.";
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
+
+
+const User = new mongoose.model("User", userSchema);
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+
+app.get("/register", (req, res) => {
+    res.render("register");
+});
+
+app.post("/register", (req, res) => {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+
+    newUser.save().then(() => {
+        res.render("secrets");
+    }).catch((error) => {
+        console.log(error);
+    });
+});
+
+app.post("/login", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email: username}).then((foundUser) => {
+        if(foundUser) {
+            if(foundUser.password === password) {
+                res.render("secrets");
+            }
+        }
+    }).catch((error) => {
+        console.log(error);
+    })
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
